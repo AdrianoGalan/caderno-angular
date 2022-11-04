2e3cf777806d8008beea8a56f1d47e12a76ffd98
Adriano Galan <dricoo@hotmail.com>
2022-11-04 08:27:20 -0300

    listando maquinas

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,3 +1,6 @@
+import { Router } from '@angular/router';
+import { MaquinaService } from './../../maquina/maquina.service';
+import { Maquina } from 'src/app/model/maquina';
 import { Component, OnInit } from '@angular/core';
 
 
@@ -11,34 +14,31 @@ export class HomeComponent implements OnInit {
 
 
 
-
-
-
-
-
-
+  dataSource: Maquina[] = [];
+  displayedColumns: string[] = ['sigla'];
 
 
 
   constructor(
+    private service: MaquinaService,
+    private router: Router
 
   ) {
 
-
-
-
-
   }
-
-
-
 
   ngOnInit(): void {
 
-
+    this.service.list().subscribe({ next: (res) => this.dataSource = res })
 
   }
 
+  onClick(maquina: any) {
+
+    localStorage.setItem('sigla', maquina.sigla)
+    this.router.navigate(['maquina/detalhe']);
+
+  }
 
 
 
