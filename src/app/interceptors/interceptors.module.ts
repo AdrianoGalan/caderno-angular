e00d0aa07f262cbc8de032f72ff3eb82d3e6b321
Adriano Galan <dricoo@hotmail.com>
2022-11-10 10:09:31 -0300

    tratando erro 403

--- src/app/interceptors/interceptors.module.ts
+++ src/app/interceptors/interceptors.module.ts
@@ -1,3 +1,4 @@
+import { ErroInterceptor } from './erro.interceptor';
 import { AuthInterceptor } from './auth.interceptor';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -11,7 +12,8 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
     CommonModule
   ],
   providers: [
-    AuthInterceptor,{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi:true}
+    AuthInterceptor,{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi:true},
+    ErroInterceptor,{provide: HTTP_INTERCEPTORS, useClass: ErroInterceptor, multi:true}
   ],
 })
 export class InterceptorsModule { }
